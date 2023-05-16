72499ee057dd3ab4421904e2935cef99b6a1e727
denn1s <dennis.aldana@gmail.com>
2023-05-16 12:00:40 -0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,14 +8,14 @@ const app = express();
 const router = express.Router();
 
 // Define a route that responds with a JSON object when a GET request is made to the root path
-router.get("/", (req, res) => {
+router.get("/hi", (req, res) => {
   res.json({
     hello: "hi!"
   });
 });
 
 // Use the router to handle requests to the `/.netlify/functions/api` path
-app.use(`/.netlify/functions/api`, router);
+app.use(`/api/hi`, router);
 
 // Export the app and the serverless function
 module.exports = app;
