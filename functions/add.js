8884903b6cdd5cc2b2f4c848dd6c11316890165f
denn1s <dennis.aldana@gmail.com>
2023-05-16 11:59:00 -0600

    Update add.js

--- functions/add.js
+++ functions/add.js
@@ -1,3 +1,4 @@
+/*
 exports.handler = async (event, context) => {
   const { num1, num2 } = JSON.parse(event.body)
   const result = num1 + num2
@@ -6,4 +7,23 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({ result }),
   }
+}
+*/
+
+exports.handler = async (event) => {
+  const { num1, num2 } = event.queryStringParameters
+  
+  // Perform the addition
+  const result = parseInt(num1) + parseInt(num2)
+  
+  // Prepare the JSON response
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ result }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  
+  return response;
 }
