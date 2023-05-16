de02fb96c75aaf2175b16c43c0e9c34844bb50e6
Dennis Aldana <dennis.aldana@gmail.com>
2023-05-16 16:53:49 -0600

    fix: move sensitive data to env

--- functions/getPokemon.cjs
+++ functions/getPokemon.cjs
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 
 exports.handler = async () => {
   const dbConfig = {
-    host: 'aws.connect.psdb.cloud',
-    port: 3306,
-    user: 'it4g67yoskyjxmk6rjc6',
-    password: 'pscale_pw_cykUTCkqvlmoVjseeeQrUWsmJcY3NXOQpUGqNSxdLi2',
-    database: 'test-database',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
       rejectUnauthorized: true,
     },
